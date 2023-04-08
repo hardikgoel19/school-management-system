a086a8d1de3f6df2d7fc1c56e76b7501ef058296
Utsav Dikshit <utsavdikshit675@gmail.com>
2023-04-08 16:11:05 +0530

    added lombok

--- src/main/java/com/school/management/model/StudentPersonal.java
+++ src/main/java/com/school/management/model/StudentPersonal.java
@@ -5,8 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "student_personal")
+@Getter
+@Setter
+@NoArgsConstructor
 public class StudentPersonal {
 	
 	@Id
@@ -58,134 +65,5 @@ public class StudentPersonal {
 	@Column(name = "father_occupation")
 	private String fatherOccupation;
 	
-	public StudentPersonal() {}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getMiddleName() {
-		return middleName;
-	}
-
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddressId() {
-		return addressId;
-	}
-
-	public void setAddressId(String addressId) {
-		this.addressId = addressId;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getContact() {
-		return contact;
-	}
-
-	public void setContact(String contact) {
-		this.contact = contact;
-	}
-
-	public String getAadhar() {
-		return aadhar;
-	}
-
-	public void setAadhar(String aadhar) {
-		this.aadhar = aadhar;
-	}
-
-	public String getDob() {
-		return dob;
-	}
-
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-
-	public String getDateOfAdmission() {
-		return dateOfAdmission;
-	}
-
-	public void setDateOfAdmission(String dateOfAdmission) {
-		this.dateOfAdmission = dateOfAdmission;
-	}
-
-	public String getMotherName() {
-		return motherName;
-	}
-
-	public void setMotherName(String motherName) {
-		this.motherName = motherName;
-	}
-
-	public String getMotherMobile() {
-		return motherMobile;
-	}
-
-	public void setMotherMobile(String motherMobile) {
-		this.motherMobile = motherMobile;
-	}
-
-	public String getMotherOccupation() {
-		return motherOccupation;
-	}
-
-	public void setMotherOccupation(String motherOccupation) {
-		this.motherOccupation = motherOccupation;
-	}
-
-	public String getFatherName() {
-		return fatherName;
-	}
-
-	public void setFatherName(String fatherName) {
-		this.fatherName = fatherName;
-	}
-
-	public String getFatherMobile() {
-		return fatherMobile;
-	}
-
-	public void setFatherMobile(String fatherMobile) {
-		this.fatherMobile = fatherMobile;
-	}
-
-	public String getFatherOccupation() {
-		return fatherOccupation;
-	}
-
-	public void setFatherOccupation(String fatherOccupation) {
-		this.fatherOccupation = fatherOccupation;
-	}
-	
+		
 }
